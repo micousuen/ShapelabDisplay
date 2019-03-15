9ca7cecbc72d873f198aee3a65f08edd1ef4d4e6
micousuen <micousuen@outlook.com>
2019-03-15 11:17:18 -0400

    Add docker startup

---
+++ mongo-init.js
@@ -0,0 +1,14 @@
+db.auth('shapelabAdmin', 'shapelabPassword')
+
+db.createUser(
+	{
+		user:"shapelab",
+		pwd:"shapelab",
+		roles: [{role:"readWrite",db: "shapelab"}, {role:"readWrite", db: "admin"}],
+		passwordDigestor:"server"
+	}
+);
+db.auth("shapelab", "shapelab");
+db = db.getSiblingDB('shapelab');
+db.createCollection("userList");
+db.userList.insert({"username":"admin", "password":"admin", "role":"role"});
