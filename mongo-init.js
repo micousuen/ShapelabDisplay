61930afc74127c9b724f3af492d8dca6614244de
micou <micousuen@outlook.com>
2019-03-25 09:53:27 -0400

    typo fix

--- mongo-init.js
+++ mongo-init.js
@@ -11,4 +11,4 @@ db.createUser(
 db.auth("shapelab", "shapelab");
 db = db.getSiblingDB('shapelab');
 db.createCollection("userList");
-db.userList.insert({"username":"admin", "password":"admin", "role":"role"});
+db.userList.insert({"username":"admin", "password":"admin", "role":"admin"});
